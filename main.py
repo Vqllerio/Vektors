df6892cc5ea463128975d708f715dad98390e641
Vallerio <vrayford@student.ciputra.ac.id>
2024-11-24 20:48:04 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
 from penjumlahan import jumlah
+from pengurangan import 
+from panjangvektor import panjangV
 print("Operasi Pada vektor")
 print("1. Penjumlahan")
 print("2. Pengurangan")
+print("3. Panjang")
 
 pilih = input("pilih : ")
 
+x1 = int(input("componen X untuk Vektor 1 : "))
+y1 = int(input("componen y untuk Vektor 1 : "))
+x2 = int(input("componen X untuk Vektor 2 : "))
+y2 = int(input("componen y untuk Vektor 2 : "))
+A = [x1, y1]
+B = [x2, y2]
+
 if pilih == '1':
-    x1 = int(input("componen X untuk Vektor 1 : "))
-    y1 = int(input("componen y untuk Vektor 1 : "))
-    x2 = int(input("componen X untuk Vektor 2 : "))
-    y2 = int(input("componen y untuk Vektor 2 : "))
-    A = [x1, y1]
-    B = [x2, y2]
     C = jumlah(A, B)
     print(f'{A} + {B} = {C}')
+
+elif pilih == '2':
+    C = 
+
+elif pilih == '3':
+    C = panjangV(A, B)
+    print(f'(({A}^2 + {B}^2)^0.5 = {C}')
+    
